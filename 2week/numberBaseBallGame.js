7b42d7b1d515f0e0c0cf0f2f28aa28a317b847e0
parkbark <020psj070@gmail.com>
2025-07-10 01:46:37 +0900

    숫자 야구 게임 5단계

--- 2week/numberBaseBallGame.js
+++ 2week/numberBaseBallGame.js
@@ -20,21 +20,14 @@ function createRandomNum(min, max) {
 
 //입력 숫자 제한하는 함수
 function onlyNumAndComma(obj) {
-    let val = obj.value; //입력되고 있는 값
-    val = val.replace(/[^0-9,]/g, ''); //숫자와 콤마 빼고는 전부 '' <- 대체
-    const comma = val.toString().replace(/\B(?<!\.\d*)(?=(\d{1})+(?!\d))/g, ","); //1번마다 콤마 추가하는 정규식
-    const  threeNum= comma.substr(0,5); // 5번째 인덱스 이후로 문자열 다 자름 ex) 1,2,3 (세자리숫자만 표기)
-    obj.value = threeNum; //입력창에 있는 입력값에 반영함
-}
-
-//사용자한테 숫자 3개 입력 받는 함수
-function inputNumber(inputThreeNum, inputCount) {
-    list = inputThreeNum.split(',').map(n => parseInt(n)); //입력받은 숫자를 , 기준으로 나눠서 배열에 담음(숫자로 형변환해서) 
-    return compareNumber(inputThreeNum, inputCount); //숫자 비교 함수를 호출하고 그 결과를 반환
+    let val = obj.value; //입력값 가져오기
+    val = val.replace(/[^0-9]/g, ''); // 숫자만 남기고 제거
+    val = val.substr(0, 1); // 첫 자리 숫자만 남김
+    obj.value = val; // 입력창에 반영
 }
 
 //입력 받은 숫자랑 랜덤 숫자 비교하는 함수
-function compareNumber(inputThreeNum, inputCount) {
+function compareNumber(list, inputCount) {
     let strike=0;
     let ball=0;
     for(let i=0; i<3; i++) {
@@ -56,54 +49,95 @@ function compareNumber(inputThreeNum, inputCount) {
             resultObj.result = "pass"; //중간 단계에 못 맞췄을 때 상태:pass
         }
     }
+    inputThreeNum = list.join(',');
     resultObj.msg = `${inputCount}회차 : ${inputThreeNum} --> ${strike} 스트라이크 , ${ball} 볼`; //msg 추가
     return resultObj; //결과 객체 반환
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     rdList = createRandomNum(1,9); //1부터 9까지 숫자 내에서 생성
-    console.log("rdList: ", rdList);
     
     let userName = document.getElementById("userName"); //사용자 이름
     let randomNum = document.getElementById("randomNum"); //랜덤 숫자 input창
-    let inputNum = document.getElementById("inputNum"); //사용자 숫자 입력 input창
+    let inputNum1 = document.getElementById("inputNum1"); //사용자 숫자 입력 input창 -1
+    let inputNum2 = document.getElementById("inputNum2"); //사용자 숫자 입력 input창 -2
+    let inputNum3 = document.getElementById("inputNum3"); //사용자 숫자 입력 input창 -3
     let gameResultText = document.getElementById("gameResultText"); //결과메시지 출력할 부분
     let textArea = document.getElementById("textArea"); //게임 결과(resultobj.msg) 출력할 textarea
     
-    inputNum.setAttribute("readonly", "readonly"); //기본적으로 readonly로 막아둠
+    inputNum1.setAttribute("readonly", "readonly"); //기본적으로 readonly로 막아둠
+    inputNum2.setAttribute("readonly", "readonly"); //기본적으로 readonly로 막아둠
+    inputNum3.setAttribute("readonly", "readonly"); //기본적으로 readonly로 막아둠
 
     userName.addEventListener("keyup", function (e) { //사용자 이름 입력 후
         if (e.key === 'Enter') { //엔터쳤을 때
             if (userName.value) { //값이 있다면
-                inputNum.removeAttribute("readonly"); //사용자 숫자 입력 input창 readonly 속성 삭제
-                inputNum.focus(); //사용자 숫자 입력 input창에 포커스
+                randomNum.value = rdList.join(','); //랜덤리스트에 있는 값을 ,로 묶어서 문자열 만들고 그걸 랜덤 숫자 input창에 출력
+                inputNum1.removeAttribute("readonly"); //사용자 숫자 입력 input1창 readonly 속성 삭제
+                inputNum1.focus(); //사용자 숫자 입력 input1창에 포커스  
+            }
+        }
+    });
+
+    inputNum1.addEventListener("keyup", function (e) { //input1 입력 후
+        if (e.key === 'Enter') { //엔터쳤을 때
+            if (inputNum1.value) { //값이 있다면
+                inputNum2.removeAttribute("readonly"); //사용자 숫자 입력 input2창 readonly 속성 삭제
+                inputNum2.focus(); //사용자 숫자 입력 input2창에 포커스
+            }
+        }
+    });
+
+    inputNum2.addEventListener("keyup", function (e) { //input2 입력 후
+        if (e.key === 'Enter') { //엔터쳤을 때
+            if (inputNum2.value) { //값이 있다면
+                inputNum3.removeAttribute("readonly"); //사용자 숫자 입력 input3창 readonly 속성 삭제
+                inputNum3.focus(); //사용자 숫자 입력 input3창에 포커스
             }
         }
     });
 
     let count =0; //입력 횟수 카운트 할 변수
+    document.getElementById("btn").addEventListener('click', function() { //숫자 맞추기 버튼 클릭 시
+        list = []; //list 초기화
+        list.push(parseInt(inputNum1.value));
+        list.push(parseInt(inputNum2.value));
+        list.push(parseInt(inputNum3.value)); //list에 사용자 입력 값을 담음
+        count +=1; //카운트 +1
+        const status = compareNumber(list, count); //resultobj를 가져옴
+        let beforeText = textArea.value; //기존에 있던 textarea 값들
+        let newText = beforeText + '\n' + status.msg; //기존값+새로운값
+        textArea.value = newText; //새로운 값으로 출력
 
-    inputNum.addEventListener("keyup", function(e) { //숫자 입력창 입력 후
-        if (e.key === 'Enter') { //엔터쳤을 때
-            count +=1; //카운트 +1
-            const status = inputNumber(inputNum.value, count); //resultobj를 가져옴
-            let beforeText = textArea.value; //기존에 있던 textarea 값들
-            let newText = beforeText + '\n' + status.msg; //기존값+새로운값
-            textArea.value = newText; //새로운 값으로 출력
-            if (status.result === "success") { //게임 결과가 success일 때
-                randomNum.value = rdList.join(','); //랜덤리스트에 있는 값을 ,로 묶어서 문자열 만들고 그걸 랜덤 숫자 input창에 출력
-                gameResultText.innerText = "스트라이크 3번 성공하였습니다. 축하드립니다!!"; //결과메시지 출력
-                inputNum.setAttribute("readonly", "readonly"); //다시 readonly 속성 추가
-                inputNum.value = ''; //사용자 숫자 input창 빈창으로 변경
-            } else if (status.result === "false") { //게임 결과가 false일 때
-                randomNum.value = rdList.join(',');
+        if(status.result === "pass") { //결과가 pass일 때
+            inputNum1.value = '';
+            inputNum2.value = '';
+            inputNum3.value = ''; 
+            inputNum1.focus(); //다시 입력할 수 있게 포커스 설정
+        } else { //결과가 pass가 아닐 때(success or false)
+            if (status.result === "success") { //성공 메시지 출력
+                gameResultText.innerText = "스트라이크 3번 성공하였습니다. 축하드립니다!!";
+            } else { //실패 메시지 출력
                 gameResultText.innerText = "조금 더 분발하세요!! 다음 기회에...";
-                inputNum.setAttribute("readonly", "readonly");
-                inputNum.value = '';
-            } else { //게임 결과가 pass일 떄
-                inputNum.value = ''; 
-                inputNum.focus(); //다시 입력할 수 있게 포커스 설정
             }
+
+            inputNum1.setAttribute("readonly", "readonly"); //다시 readonly 속성 추가
+            inputNum2.setAttribute("readonly", "readonly"); //다시 readonly 속성 추가
+            inputNum3.setAttribute("readonly", "readonly"); //다시 readonly 속성 추가
+            
+            inputNum1.value = ''; //사용자 숫자 input1창 빈창으로 변경
+            inputNum2.value = ''; //사용자 숫자 input2창 빈창으로 변경
+            inputNum3.value = ''; //사용자 숫자 input3창 빈창으로 변경
+
+            setTimeout(() => {
+                if (confirm("게임을 다시 하시겠습니까?")) {
+                    window.location.reload(); //확인을 누르면 화면 새로고침해서 다시 시작됨
+
+                    ///////
+                    // 사용자 정보 그대로 가지고 와서 걍 바로 숫자 입력할 수 있게 해도 좋을 것 같음 tobe..
+                    ///////
+                }
+            }, 1000); //1초 후에 실행
         }
     });
 });
